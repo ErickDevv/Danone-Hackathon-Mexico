f95ffb02dfbbf1fc23053c09f0ab098c6ad8d1bd
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-06-19 06:00:30 -0600

    Update ecodanone.tsx

--- frontend/src/pages/ecodanone.tsx
+++ frontend/src/pages/ecodanone.tsx
@@ -1,8 +1,8 @@
 import Nav from "./nav";
-import arrozconleche from "/public/arrozconleche.jpg";
-import flan from "/public/flan.jpg";
-import cuajada from "/public/cuajada.jpg";
-import griego from "/public/griego.jpg";
+import arrozconleche from "../../public/arrozconleche.jpg";
+import flan from "../../public/public/flan.jpg";
+import cuajada from "../../public/public/cuajada.jpg";
+import griego from "../../public/public/griego.jpg";
 
 import { useQuery } from "react-query";
 import { useState } from "react";
