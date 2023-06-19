384377d014bdc9d28c9cfff9a60107fd554b5bfc
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-06-19 04:06:10 -0600

    cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 
 
 app.get("/api/getProducts", (req, res) => {
