05f50c550610b8c31b331d8a4b19c06874ff493f
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-06-19 03:06:55 -0600

    nav improvements

--- frontend/src/pages/nav.tsx
+++ frontend/src/pages/nav.tsx
@@ -1,16 +1,27 @@
 import logoNav from "/public/logoNav.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
+  const navigate = useNavigate();
   return (
     <div className="h-20 px-4 w-full flex  items-center justify-between bg-white shadow">
+      <button onClick={() => navigate("/")}>
       <img src={logoNav} alt="logo" className="h-10 md:h-16 self-center" />
+
+      </button>
       <div className="flex">
-        <h2 className="text-lg sm:text-3xl font-bold text-[#0A135C]">
-          Danone Eco
-        </h2>
-        <h2 className="text-lg sm:text-3xl font-bold text-[#0A135C] ml-4">
-          EcoHealth
-        </h2>
+        <Link
+          to="/ecodanone"
+          className="text-lg sm:text-3xl font-bold text-[#0A135C]"
+        >
+          🌱DanoneEco
+        </Link>
+        <Link
+          to="/ecohealth"
+          className="text-lg sm:text-3xl font-bold text-[#0A135C] ml-4"
+        >
+          ❤️‍🩹EcoHealth
+        </Link>
       </div>
     </div>
   );
